fcf2a0dbcbb21769812f72ff9cfdce3fc29da0b0
kstreigh <kstreigh@iu.edu>
2024-02-13 14:24:56 -0500

    fix

--- valentine/server/server.js
+++ valentine/server/server.js
@@ -3,7 +3,8 @@ require("dotenv").config({ path: "./config.env" });
 const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
-app.use(require("./routes/record"));
+
+app.use(express.static("./client/build"))
 // get driver connection
 const dbo = require("./db/conn");
 app.listen(port, () => {
