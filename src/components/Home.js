6d102cd1125a7e80a178b3d2291c528530032cc7
kstreigh <kstreigh@iu.edu>
2024-02-13 22:24:32 -0500

    fix

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,12 +6,12 @@ const Home = () => {
     return (
       <div>
         <div className="homeContainer">
-          <h1 className="homeTitle">Happy Valentines Day Loggan!</h1>
+          <h1 className="homeTitle">Happy Valentines Day Loggan! I LOVE YOU!!!</h1>
           <div className ="container">
           </div>
 
           </div>
-          <h1 className="homeTitle">Pictures of Us!!!! </h1>
+          <h1 className="homeTitle">Pictures of Me and You!!!! </h1>
 
           <div className="imageContainer">
           <div className ="container1">
@@ -21,11 +21,24 @@ const Home = () => {
           <div className ="container3">
           </div>
           </div>
-          <h1 className="homeTitle">Things I Love About You!</h1>
+          <h1 className="homeTitle">My Letter To You!!!!!!!!</h1>
           <h2 className="card">
-            1. You are very handsome. You have a pretty face hehe<br />
-            2. You are a big sweetie. I appreciate you always driving to see me <br />
-            3. You are the best boyfriend! I love talking to you each and every day<br />
+            Dear Loggan<br />
+            Happy Valentines Day baby! I hope that my website is working okay haha...
+            Thank you for being the best boyfriend ever! I really appreciate you driving to come see me every day and 
+            paying for our dates as well as making me laugh all the time. 
+            I love you very much and I have loved getting to know you and I really hope you know I love and appreciate 
+            everything you do for me. 
+            Some things I find attractive about you... well for one we actually have so much in common.
+            I was so excited to learn that you like Valorant and Pokemon. You are also actually kind of a romantic at heart 
+            even if you dont readily admit it. Youre really funny and I admire how likable you are. Youre so determined in 
+            life and I can tell youre meant to be a great dad. 
+            Another thing I appreciate is that you are always thinking of my feelings and what my feelings are for you. 
+            Theres not many men in the world like you and I truly believe that. I am one lucky lucky girl. 
+            Thank you so much for giving me a chance and letting me get to know you. 
+            I hope you feel the same way that I do and I cant wait to see what the future will be like for us!!! <br />
+            I love you <br />
+            Kelly 
           </h2>
       </div>
     );
